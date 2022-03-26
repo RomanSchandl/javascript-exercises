0d1790bc8430501c364d3f421c399a3b97e16132
Roman <roman.schandl@gmail.com>
2022-03-26 21:37:13 +0100

    Finished exercise "reverse String" with Array.from() method

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function (originalString) {
+  let stringArray = Array.from(originalString);
+  let stringArrayreversed = stringArray.reverse();
+  let reversedString = stringArrayreversed.join("");
+  return reversedString;
 };
+
+reverseString("Hello");
 
 // Do not edit below this line
 module.exports = reverseString;
