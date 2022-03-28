4321ca1c063cd7bcb74c86695c776b56e4d71763
Roman <roman.schandl@gmail.com>
2022-03-28 10:30:42 +0200

    1st try with for each loop - failed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, arg) {
+  // 1st TRY with an for each loop
+  //   array.forEach((element) => {
+  //     if (element == arg) {
+  //       array[element].pop();
+  //     }
+  //   });
+  //   return array;
 };
+
+// removeFromArray([1, 2, 3, 4], 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
